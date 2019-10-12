f92122e9f24e486c93eff01c0986113610419dc9
pgmgent-EMMELINEMARTENS <emmemart1@student.arteveldehs.be>
2019-10-12 23:02:44 +0200

    oefening2

---
+++ lessen/les3/oefening2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace oefening2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // DEEL A: Tafel van 5 -> loopen van 1 tot 10 
+        int tafel = 5;
+        // int i =5 _> code gaat niet werken omdat we twee kkeer dezelfde variabel gaan declareren 
+        for(int i = 0;i <= 10; i++){
+            Console.WriteLine("" + i + "x" + "" + tafel + " = " + i*tafel);
+        }
+        // DEEL B: Alle tafels van vermenigvuldigen
+        for(int i = 0; i <= 10; i++){
+        for(int j = 0;j < i; j++){
+            Console.WriteLine("" + j + "x" + "" + i + " = " + j*i);
+        }
+}
+// DEEL C: Zelf waarde ophalen
+Console.WriteLine("Geef een getal in van 1 tot 10");
+int specifieketafel= Convert.ToInt32(Console.ReadLine());
+
+               for(int i = 0; i <= 10; i++){
+        
+            Console.WriteLine("" + i + "x" + "" + specifieketafel+ " = " + specifieketafel*i);
+        }
+}
+}
+};
