761f54c83446ba0ef9cfb74a2de2e174667f1bf8
pgmgent-EMMELINEMARTENS <emmemart1@student.arteveldehs.be>
2019-10-13 15:18:16 +0200

    oefening5 af

--- lessen/les3/oefening5/Program.cs
+++ lessen/les3/oefening5/Program.cs
@@ -7,22 +7,24 @@ namespace oefening5
         static void Main(string[] args)
         {
             Console.WriteLine("Geef een lengte weer van de fibonnaci reeks");
+            // ask for a value 
+            int valuelength = Convert.ToInt16(Console.ReadLine());
 
-            long valuelength = Convert.ToInt16(Console.ReadLine());
-
-            Console.WriteLine(fibonnaciFun(valuelength));
+            fibonnaciFun(valuelength);
            /*De volgende waarde is de som van de 2 vorige waardes -> fibonnaci
            Starten van 0 en zo naar oneindig
            n defineren als een lengte
            */
         }
-        static public long fibonnaciFun(long valuelength)
-        {   Console.WriteLine("...............................");
+            static public void fibonnaciFun(int valuelength)
+        {
+            // make a static function that is empty
+            //declared the values
             int value1 = 0;
             int value2 = 1;
         
-            Console.WriteLine(value1);
-            for(int i = 0; i <= valuelength; i++ )
+
+            for(int i = 1; i <= valuelength; i++ )
             {
                 int number1 = value1;
                 int result = value1 + value2;
@@ -30,7 +32,7 @@ namespace oefening5
                 value1 = value2;
                 value2 += number1;
             }
-            Console.WriteLine("...............................");
+            
         
         }
         
